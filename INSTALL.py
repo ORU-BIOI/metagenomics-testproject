d195a7b3c720349d555087b589e61c7914fba7d5
Brynjar Smari Bjarnason <binni@binnisb.com>
2015-02-16 16:50:48 +0100

    fixed for python 2.6.6

--- INSTALL.py
+++ INSTALL.py
@@ -3,14 +3,14 @@ from __future__ import print_function
 import re
 import sys
 #Get Python Version
-version = sys.version_info.major
+version = sys.version_info[0]
 if version == 2:
 	from urllib import urlopen
 elif version == 3:
 	from urllib.request import urlopen
 
 miniconda_base = "http://repo.continuum.io/miniconda/"
-miniconda = "Miniconda{}-\d+\.\d+\.\d+-Linux-x86_64.sh".format('' if version == 2 else version)
+miniconda = "Miniconda{0}-\d+\.\d+\.\d+-Linux-x86_64.sh".format('' if version == 2 else version)
 
 r = urlopen(miniconda_base)
 text = r.read().decode("utf-8")
