b76c7e9bec4833241e35f923a62fbbd3def6a228
Brynjar Smari Bjarnason <binni@binnisb.com>
2015-03-24 15:31:58 +0100

    add scheduling files

---
+++ scheduling/scheduler.sh
@@ -0,0 +1,13 @@
+# If Snakefile in same directory, run so:
+# $ ./scheduler.sh rule1 rule2
+# If snakemake in another directory:
+# $ ./scheduler.sh -s ../Snakefile rule1 rule2
+# you can even pass in parameters:
+# $ ./scheduler.sh -n -s ../Snakefile rule1 rule2
+snakemake -j 350000 --debug -p \
+          --rerun-incomplete \
+	  --immediate-submit \
+	  --cluster './scheduler.py {dependencies}' \
+	  --configfile ./scheduler.json \
+	  -s ./Snakefile \
+	  $*
